3199fac5569b0c7ac31ec573857be7a795ef805a
kajeta <ezikeohaking@gmail.com>
2022-09-30 05:48:09 +0100

    react-native teslaClone

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,ImageBackground, requireNativeComponent } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header/>
+      <CarsList/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
